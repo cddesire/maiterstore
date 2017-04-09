7fa05bdabced7035ddb46d94b6a7f30a62033f37
Aspirin Vagrant <cdaspirin@gmail.com>
2017-04-09 15:36:13 +0800

    g_func

--- src/examples/con_component.cc
+++ src/examples/con_component.cc
@@ -1,26 +1,19 @@
 #include "client/client.h"
 
-
 using namespace dsm;
 
-//DECLARE_string(graph_dir);
 DECLARE_string(result_dir);
 DECLARE_int64(num_nodes);
 DECLARE_double(portion);
 
 struct PagerankIterateKernel : public IterateKernel<int, int, vector<int> > {
     int zero;
-
-
     PagerankIterateKernel() : zero(0){}
-
     void read_data(string& line, int& k, vector<int>& data){
         string linestr(line);
         int pos = linestr.find("\t");
         if(pos == -1) return;
-        
         int source = boost::lexical_cast<int>(linestr.substr(0, pos));
-
         vector<int> linkvec;
         string links = linestr.substr(pos+1);
         int spacepos = 0;
@@ -32,35 +25,33 @@ struct PagerankIterateKernel : public IterateKernel<int, int, vector<int> > {
             links = links.substr(spacepos+1);
             linkvec.push_back(to);
         }
-
         k = source;
         data = linkvec;
     }
 
     void init_c(const int& k, int& delta, vector<int>& data){
-            int  init_delta = k;
-            delta = init_delta;
+        int  init_delta = k;
+        delta = init_delta;
     }
 
     void init_v(const int& k,int& v,vector<int>& data){
-            v=0;
+        v=0;
     }
+
     void accumulate(int& a, const int& b){
-            a=std::max(a,b);
+        a=std::max(a,b);
     }
 
     void priority(int& pri, const int& value, const int& delta){
-            pri = value-std::max(value,delta);
+        pri = value-std::max(value,delta);
     }
 
     void g_func(const int& k,const int& delta, const int& value, const vector<int>& data, vector<pair<int, int> >* output){
-            int outv = value;
-            
-            //cout << "size " << size << endl;
-            for(vector<int>::const_iterator it=data.begin(); it!=data.end(); it++){
-                    int target = *it;
-                    output->push_back(make_pair(target, outv));
-            }
+        int outv = value;
+        for(vector<int>::const_iterator it=data.begin(); it!=data.end(); it++){
+            int target = *it;
+            output->push_back(make_pair(target, outv));
+        }
     }
 
     const int& default_v() const {
@@ -75,18 +66,13 @@ static int Pagerank(ConfigData& conf) {
                                         new Sharding::Mod,
                                         new PagerankIterateKernel,
                                         new TermCheckers<int, int>::Diff);
-    
-    
     kernel->registerMaiter();
-
     if (!StartWorker(conf)) {
         Master m(conf);
         m.run_maiter(kernel);
     }
-    
     delete kernel;
     return 0;
 }
 
 REGISTER_RUNNER(Pagerank);
-
